6b0b57383b645ede03858a7405b776550dbd33fc
Pappaman1 <zmw7710@g.rit.edu>
2018-01-29 04:24:58 -0500

    Add files via upload
    Heroku just doesnt work

---
+++ src/htmlResponses.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+// pull in the file system module
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const bling = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const bird = fs.readFileSync(`${__dirname}/../client/client3.html`);
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const getBling = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(bling);
+  response.end();
+};
+
+const getBird = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(bird);
+  response.end();
+};
+
+module.exports.getIndex = getIndex;
+module.exports.getIndex = getBling;
+module.exports.getIndex = getBird;
